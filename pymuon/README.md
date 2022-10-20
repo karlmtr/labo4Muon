a79865dd658eb44ccd8da1b0d5564a5b2edb3728
schmdr <sacha@medaer.me>
2022-10-20 18:09:15 +0200

    Basic architecture of pymuon, python app for muon attenuatino simulation

---
+++ pymuon/README.md
@@ -0,0 +1,16 @@
+# Need to be Written
+
+This folder contains a python application which allows one to simulate
+the attenuation of a given muon through layers of different materials.
+
+## How to run the program ?
+Install locally the program as follows:
+```sh
+python3 -m pip install -e pymuon
+```
+And don't forget to install the dependencies:
+```sh
+python3 -m pip install -r requirements.txt
+```
+
+Create your code into the *examples/* folder.
---
+++ pymuon/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymuon",
+    version="0.1.0",
+    author="Sacha Medaer, Edward Galantay, Brayan Reyes Alvarado",
+    author_email="sacha.medaer@etu.unige.ch",
+    python_requires=">=3.9.0",
+    description="Muon Attenuation Simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='DWTFYW',
+    include_package_data=True,	# controls whether non-code files are copied when package is installed
+    install_requires=["scipy", "numpy", "matplotlib"],
+    classifiers=[
+        "Natural Language :: English",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
