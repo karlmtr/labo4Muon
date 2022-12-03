c48decf350abddf967e95e412c2979f70d4cf8e9
Karlmtr <31413546+karlmtr@users.noreply.github.com>
2022-12-03 12:05:42 +0100

    Removed character

--- calibration/runCalibration.sh
+++ calibration/runCalibration.sh
@@ -2,5 +2,5 @@ echo "No,A,B" > "calibrationValues.txt"
 for i in {1..6}
 do 
     echo $i
-    echo $(python3 linear_fits.py ../data/calibration/ $i & ) >> "calibrationValues.txt"
+    echo $(python3 linear_fits.py ../data/calibration/ $i) >> "calibrationValues.txt"
 done
