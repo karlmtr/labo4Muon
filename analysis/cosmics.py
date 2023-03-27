0127eca454a230cf920281bd508ee493a6a2349e
Edward Galantay <31413546+karlmtr@users.noreply.github.com>
2023-03-27 12:21:40 +0200

    added function to display ADC distribution for one type of event / and display the number of events for each types

--- analysis/cosmics.py
+++ analysis/cosmics.py
@@ -4,10 +4,10 @@ import pathlib
 import matplotlib.pyplot as plt
 
 pmtValues = [f"pmt{i}" for i in range(1,9)]
-pmtReg = [f"pmt_reg{i}" for i in range(1,9)]
+# pmtReg = [f"pmt_reg{i}" for i in range(1,9)]
 
 def load_data(path : pathlib.Path) -> pd.DataFrame :
-    return pd.read_csv(path, delimiter="\t", names=pmtValues + pmtReg)
+    return pd.read_csv(path, delimiter="\t", names=["time", "trigger"] + pmtValues)
 
 def get_number_events(df: pd.DataFrame, threshold:int = 50) -> int : 
     """
@@ -34,13 +34,60 @@ def nbEvents_vs_th(df):
     plt.xlabel("threshold")
     plt.ylabel("# events")
     plt.show()
-           
+
+
+def nbEvents_for_combinedPMTs(df:pd.DataFrame):
+    """
+    "Good events ?"
+    ----------------
+    pmt1 pmt2 pmt3 pmt4 pmt5 pmt6 pmt7 pmt8
+    1    1    0    0    0    0    0    0    -> 192 (binary to decimal conversion)
+    1    1    1    0    0    0    0    0    -> 224
+    1    1    1    1    0    0    0    0    -> 240
+    1    1    1    1    1    0    0    0    -> 248
+    1    1    1    1    1    1    0    0    -> 252
+    1    1    1    1    1    1    1    0    -> 254
+    1    1    1    1    1    1    1    1    -> (2^8) - 1 = 255
+
+    """
+    # events that make sense ?
+    wanted_events = [192,224, 240, 248, 252, 254, 255]
+    # af = df[(df["trigger"] == 192) |
+    #         (df["trigger"] == 224) |
+    #         (df["trigger"] == 240) |
+    #         (df["trigger"] == 248) |
+    #         (df["trigger"] == 252) |
+    #         (df["trigger"] == 254) |
+    #         (df["trigger"] == 255) 
+    #         ]
+    nbs = [len(df[df["trigger"] == nb]) for nb in wanted_events] 
+    
+    df = df[df["trigger"] == 255 ] # on choisit le 
+    for pmtName in pmtValues: 
+        fig,ax = plt.subplots()
+        ax.hist(df[pmtName], bins=300, range=[0,1300])
+        ax.set(xlabel="ADC", ylabel="# events", title=f"ADC distribution for {pmtName.upper()}")
+        # plt.show()
+        plt.savefig(pathlib.Path(f"../pictures/{pmtName}_adc_distrib.png"))
+    print(f"Wanted events\t{wanted_events}")
+    print(f"# events\t{nbs}")
+
 
 def main() :
-    df = load_data(pathlib.Path("../data/long_run.txt"))
-    plot_adc_distribution(df)
+    df = load_data(pathlib.Path("../data/2023_03_20_run_mass_muon.txt"))
+    # plot_adc_distribution(df)
     # print("Number Cosmic Events", get_number_events(df, 50))
     # nbEvents_vs_th(df)
+    nbEvents_for_combinedPMTs(df)
+
+
+    
+    
+    
+    
+    
+    
+
 
 
 
